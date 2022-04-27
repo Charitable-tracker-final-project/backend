d86f207e5147ecfaefd0e42508adef412fe1a351
jocelyninlay <inlay.j@gmail.com>
2022-04-27 11:12:24 -0400

    more changes

--- charitable_tracker/urls.py
+++ charitable_tracker/urls.py
@@ -39,3 +39,5 @@ urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
+
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
